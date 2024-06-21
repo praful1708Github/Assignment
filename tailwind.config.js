1dbb8c1ed856006482fa3b3608540bfef51b0a2d
praful1708Github <123794402+praful1708Github@users.noreply.github.com>
2024-06-21 11:00:08 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [".src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage:{
+        'coffe-pattern':"url('/Frame 502.png')"
+      }
+    },
+  },
+  plugins: [],
+}
+
